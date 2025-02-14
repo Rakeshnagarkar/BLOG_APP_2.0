53312f8b9fcedbd9bb0a36d971ad0cac81b82abd
Rakesh Nagarkar <rakeshpardeshi2004@gmail.com>
2025-02-14 23:41:23 +0530

    "configuration changed"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,11 @@
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap"
 import tailwind from '@astrojs/tailwind';
 
-// import vercel from "@astrojs/vercel/serverless";
 
-const SERVER_PORT = 4321
-
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
-
-const LIVE_URL = "https://blog-app.github.io"
-
-const SCRIPT = Process.env.npm_lifecycle_script || ""
-const isBuild = SCRIPT.includes("astro build")
-let BASE_URL = LOCALHOST_URL
-
-if (isBuild) BASE_URL = LIVE_URL
 
 // https://astro.build/config
 export default defineConfig({
-  server: {port: SERVER_PORT},
-  site: BASE_URL,
-  base: '/blog-theme',
   integrations: [
-    sitemap(),
     tailwind({
       config: {applyBaseStyles: false}
     }),
