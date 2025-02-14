3f3c7c3291759723314cafdd9d95f057734cb299
Rakesh Nagarkar <rakeshpardeshi2004@gmail.com>
2025-02-14 23:03:43 +0530

    "astro.yml file added"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,30 @@
 import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap"
 import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+
+const SERVER_PORT = 4321
+
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+
+const LIVE_URL = "https://blog-app.github.io"
+
+const SCRIPT = Process.env.npm_lifecycle_script || ""
+const isBuild = SCRIPT.includes("astro build")
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild) BASE_URL = LIVE_URL
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel()
+  server: {port: SERVER_PORT},
+  site: BASE_URL,
+  base: '/blog-theme',
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: {applyBaseStyles: false}
+    }),
+  ],
 });
